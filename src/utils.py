4908af3e0a97ccd34d6923bee0206354f4f75348
CC Chua <chuacc@gmail.com>
2023-01-15 14:57:45 +0800

    added convert RPM

--- src/utils.py
+++ src/utils.py
@@ -37,11 +37,6 @@ def dataclean(df):
     # Convert  negative "RPM" to positive and remove the "temp" column
     df.loc[(df.temp == "Neg"), 'RPM'] = df.RPM * -1
     df.drop(columns=["temp"], inplace=True)
-
-    # Create a new column "Failure" which has represented by "0" if there are no failures and "1" if it has any one of the 5 failures
-    df["Failure"] = df["Failure A"] | df["Failure B"]| df["Failure C"]| df["Failure D"]
-
-    #Drop unwanted columns
-    df = df.drop(["Car ID", "Color", "Failure A", "Failure B", "Failure C", "Failure D", "Failure E", "Year" ] , axis = 1)
+    
     
     return df
