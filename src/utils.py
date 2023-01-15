55a3aae309e0ac2025287098bb3fb373835f9026
CC Chua <chuacc@gmail.com>
2023-01-15 15:00:58 +0800

    created failure column and removed unwanted column

--- src/utils.py
+++ src/utils.py
@@ -38,5 +38,11 @@ def dataclean(df):
     df.loc[(df.temp == "Neg"), 'RPM'] = df.RPM * -1
     df.drop(columns=["temp"], inplace=True)
     
+    # Create a new column "Failure" which has represented by "0" if there are no failures and "1" if it has any one of the 5 failures
+    df["Failure"] = df["Failure A"] | df["Failure B"]| df["Failure C"]| df["Failure D"]
+
+    # Dropped unwanted columns
+    df = df.drop(["Car ID", "Color", "Failure A", "Failure B", "Failure C", "Failure D", "Failure E", "Year" ] , axis = 1)
+
     
     return df
