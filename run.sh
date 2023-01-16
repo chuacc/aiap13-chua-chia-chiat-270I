fd01e6df33e924757c5ff088795782bc461f9298
chuacc <122468655+chuacc@users.noreply.github.com>
2023-01-16 16:47:55 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python ./src/main.py --model RandomForest
+python ./src/main.py --model LogisticRegression
+python ./src/main.py --model SVC
+python ./src/main.py --model DecisionTree
+python ./src/main.py --model GradientBoost
