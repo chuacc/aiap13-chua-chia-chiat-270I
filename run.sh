a6d8578f65fdb7d70b59488ac6b1f4524dc26f53
CC Chua <chuacc@gmail.com>
2023-02-10 07:17:02 +0800

    edit run.sh to fetch database

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+mkdir data
+wget https://techassessment.blob.core.windows.net/aiap13-assessment-data/failure.db -O .data/failure.db
 python ./src/main.py --model RandomForest
 python ./src/main.py --model LogisticRegression
 python ./src/main.py --model SVC
